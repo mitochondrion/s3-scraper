6267c325ec6aaa615492ad42e97bfd9f284ef874
mitochondrion <mitochondrion@users.noreply.github.com>
2017-11-27 01:33:58 -0500

    WIPWIPWIPWIPWIPWIP

--- downloader.py
+++ downloader.py
@@ -2,6 +2,7 @@
 
 import time
 import sys
+import os
 from collections import defaultdict
 import urllib.request
 import mutagen
@@ -18,37 +19,79 @@ else:
     pathsFile = args[2]
 
 baseUrl = f'https://s3.amazonaws.com/{bucket}/'
-print(baseUrl)
-
+destinationPath = '/Users/steakknife/Documents/cpod/'
 errors = []
-downloadCount = 0
+outFile = open("/tmp/downloader.out", 'w')
+startIndex = 0
+untilIndex = 99999
+downloadCount = startIndex
 
-outFile = open("/tmp/headers.out", 'w')
-
-paths = [line.rstrip('\n') for line in open(pathsFile)][:100]
+paths = [line.rstrip('\n') for line in open(pathsFile)][startIndex:untilIndex]
 
 for path in paths:
     downloadCount += 1
     print(str(downloadCount))
-
-    outFile.write("\n==========\n\n")
-
     url = baseUrl + path
+
     try:
         fileName, headers = urllib.request.urlretrieve(url)
     except Exception as exception:
-        msg = str(exception) + ', ' + str(exception.args) + ', ' + url
+        errorMsg = 'FAIL(urllib) ' + str(downloadCount) + ': ' + url + '\nDESC: ' + str(exception) + '\nARGS: ' + str(exception.args) + '\n\n'
+        print(errorMsg)
+        outFile.write(errorMsg)
         continue
 
-    # TODO: Check header and add to errors
-    # if headers.status_code is not 200:
-        # errorMsg = "FAIL " + str(headers.status_code) + ": " + path + ", " + fileName + "\n"
-        # outFile.write(errorMsg)
-        # print(errorMsg)
-        # continue
+    id3 = mutagen.File(fileName)
 
-    # TODO: Use ID3 to generate file name and move file
-    '''
+    try:
+        trackNumber = id3.tags['TRCK'].text[0]
+        trackAlbum = id3.tags['TALB'].text[0].replace('/', '|')
+        trackTitle = id3.tags['TIT2'].text[0].replace('/', '|')
+    except Exception as exception:
+        errorMsg = 'FAIL(id3) ' + str(downloadCount) + ': ' + str(exception) + '\nFILE: ' + fileName + '\n\n'
+        print(errorMsg)
+        outFile.write(errorMsg)
+        continue
+
+    try:
+        destinationFileName = trackNumber + ' - ' + trackTitle + ' - ' + trackAlbum + '.mp3'
+        destination = destinationPath + destinationFileName
+        os.rename(fileName, destination)
+    except Exception as exception:
+        errorMsg = 'FAIL(os) ' + str(downloadCount) + ': ' + str(exception) + '\nFILE: ' + fileName + '\n\n'
+        print(errorMsg)
+        outFile.write(errorMsg)
+        break
+
+    time.sleep(3)
+
+outFile.close
+
+
+
+##################################################
+'''
+    'TIT2'
+    ['Intermediate - Gift for a New Girlfriend?']
+    'TPE1'
+    ['ChinesePod.com']
+    'TALB'
+    ['show']
+    'TCON'
+    ['Podcast']
+    'TRCK'
+    ['3099']
+    'COMM::eng'
+    ['Learn Chinese with ChinesePod. Get professional podcasts and advanced online tools to help you learn Chinese, and connect with thousands of other Chinese learners worldwide.']
+    'TIT1'
+    ['Intermediate']
+    'USLT::eng'
+    Dialogue
+    'TDRC'
+    ['2017']
+
+========================
+
     > id3 = mutagen.File("/var/folders/_8/m8x38xmd22d_kq1drhcg0nwc0000gn/T/tmpc8qtipmm"
     > type(id3)
     <class 'mutagen.mp3.MP3'>
@@ -82,106 +125,4 @@ for path in paths:
     1  dict_keys(['TIT2', 'TPE1', 'TALB', 'TCON', 'TRCK', 'COMM::eng',         'USLT::eng', 'APIC:ChinesePod.com',  'TDRC'])
     82 dict_keys(['TIT2', 'TPE1', 'TALB', 'TCON', 'TRCK', 'COMM::eng', 'TIT1', 'USLT::eng', 'APIC:',                'TDRC'])
     17 dict_keys(['TIT2', 'TPE1', 'TALB', 'TCON', 'TRCK', 'COMM::eng', 'TIT1', 'USLT::eng',                         'TDRC'])
-    '''
-    id3 = mutagen.File(fileName)
-
-    outFile.write(path + "\n" + fileName + "\n" + headers.as_string() + "\n---\n" + str(id3.keys()) + "\n---\n\n")
-    outFile.write(str(id3))
-
-    # os.rename(fileName, destination)
-
-    time.sleep(1)
-
-outFile.close
-
-
-
-
-########################################################
-'''
-
-# def writeDiagnostic(lastResponse, finalStateInfo=''):
-    # fileTypeCountsMsg = ' | '.join([f'{key}: {fileTypeCounts[key]}' for key in fileTypeCounts])
-    # diagnosticMsg = f'Pages: {page}, Errors: {errorCount}\n\n==FINAL STATE==\n\n{finalStateInfo}\n\n==FINAL RESPONSE==\n\n{lastResponse}\n\n==HEADERS==\n\n{lastResponse.headers}\n\n==CONTENT==\n\n{lastResponse.content}\n\n==FILE TYPES==\n\nTotal: {len(files)} ({fileTypeCountsMsg})\n\n=====\n'
-    # diagnosticFile = open('./diagnostic.out', 'w')
-    # diagnosticFile.write(diagnosticMsg)
-    # diagnosticFile.close()
-
-# while page <= MAX_PAGES:
-    # print(f'Fetching page {page}...')
-
-    # response = requests.request('GET', initialUrl, params=queryParams)
-
-    # if response.status_code != 200:
-        # errorCount += 1
-        # print(f'ERROR: Received {response.status_code}\n\n==HEADERS==\n\n{response.headers}\n\n==CONTENT==\n\n{response.content}\n\n=====\n\n')
-
-        # # Backoff and retry once
-        # print('Retrying...\n\n')
-        # time.sleep(5)
-        # response = requests.request('GET', initialUrl, params=queryParams)
-
-        # if response.status_code != 200:
-            # print(f'FATAL: Failed on retry. Received {response.status_code}. Aborting.\n\n')
-            # writeDiagnostic(response, 'Failed on retry.')
-            # break
-
-    # page += 1
-
-    # root = etree.fromstring(response.content) # contentElements = root.findall('{http://s3.amazonaws.com/doc/2006-03-01/}Contents')
-
-    # for content in contentElements:
-        # sizeText = content.find('{http://s3.amazonaws.com/doc/2006-03-01/}Size').text
-        # size = int(sizeText)
-        # pathText = content.find('{http://s3.amazonaws.com/doc/2006-03-01/}Key').text
-
-        # fileType = 'NONE'
-        # pathTokens = pathText.split('.')
-        # if len(pathTokens) > 1:
-            # fileType = pathTokens[-1]
-
-            # if fileType in ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'old'] and len(pathTokens) > 2:
-                # fileType = pathTokens[-2]
-
-            # fileTypeCounts[fileType] += 1
-
-            # if fileType == 'mp3' and size > MIN_SIZE:
-                # fileTypeCounts['mp3+'] += 1
-
-        # files.append((size, fileType, pathText))
-
-    # print('Done processing page.')
-    # fileTypeCountsMsg = ' | '.join([f'{key}: {fileTypeCounts[key]}' for key in fileTypeCounts])
-    # print(f'File count: {len(files)} ({fileTypeCountsMsg})')
-
-    # # Handle paging
-    # isTruncatedText = ''
-    # isTruncatedElements = root.findall('{http://s3.amazonaws.com/doc/2006-03-01/}IsTruncated')
-    # if isTruncatedElements:
-        # isTruncatedText = isTruncatedElements[0].text
-
-    # continuationTokenElements = root.findall('{http://s3.amazonaws.com/doc/2006-03-01/}NextContinuationToken')
-
-    # if continuationTokenElements and isTruncatedText == 'true' and page < MAX_PAGES:
-        # continuationToken = continuationTokenElements[0].text
-        # queryParams['continuation-token'] = continuationToken
-    # else:
-        # diagnosticMsg = f'continuationToken count: {len(continuationTokenElements)}, isTruncated: {isTruncatedText}'
-        # writeDiagnostic(response, diagnosticMsg)
-        # break
-
-    # # Throttle otherwise AWS will start to return empty results
-    # time.sleep(THROTTLE)
-
-# filesSorted = sorted(files, key=lambda file: file[0], reverse=True)
-# print(f'Largest file: {filesSorted[0]}')
-
-# def writeFile(fileName, dataList, dataTransform):
-    # data = '\n'.join(map(dataTransform, dataList))
-    # outFile = open(fileName, 'w')
-    # outFile.write(data)
-    # outFile.close()
-
-# writeFile('./files.txt', filesSorted, str)
-# writeFile('./files.txt', filesSorted, lambda file: f'{file[0]},{file[1]},{file[2]}')
 '''
